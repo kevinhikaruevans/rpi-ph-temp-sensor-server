3ff15d360d4e53e2b5aff65ae0211a708f23ea86
Kevin Evans <evans.kevinh@gmail.com>
2022-02-02 17:11:26 -0800

    add a couple pages

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import HeaderBar from './components/HeaderBar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import DevicesPage from './pages/devices/DevicesPage';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <HeaderBar />
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/devices" element={<DevicesPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ webapp/src/pages/devices/DevicesPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DevicesPage() {
+    return "Devices";
+}
+
+export default DevicesPage;
