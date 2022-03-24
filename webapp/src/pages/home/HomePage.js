e0c4e91bf5595806d854680d84cee424a268fc8c
Kevin Evans <evans.kevinh@gmail.com>
2022-03-24 15:34:53 -0700

    new build

--- webapp/src/pages/home/HomePage.js
+++ webapp/src/pages/home/HomePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import { Divider, Typography } from '@mui/material';
 
 export default function HomePage() {
     return <>
+        <Typography variant="h3">AquaSmart</Typography>
+        <Typography variant="subtitle1">Protecting water for future generations.</Typography>
+        <Divider sx={{my: 2}}/>
         <Button variant="contained" color="primary" component={Link} to="/devices">Devices</Button>
         &nbsp;
         <Button variant="outlined" color="primary" component={Link} to="/map">Map</Button>
