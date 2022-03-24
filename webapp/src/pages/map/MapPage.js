afcfc1ecbf6482e6c0a0edf185ac87a389e09e37
Kevin Evans <evans.kevinh@gmail.com>
2022-03-24 15:37:24 -0700

    set max width, rebuild

--- webapp/src/pages/map/MapPage.js
+++ webapp/src/pages/map/MapPage.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function MapPage() {
     return <div style={{textAlign: 'center'}}>
         <Link to="/devices/1">
-            <img src={SamplePng} alt="Sample" style={{maxHeight: '100%', borderRadius: '0.5em'}} />
+            <img src={SamplePng} alt="Sample" style={{maxHeight: '100%', maxWidth: '100%', borderRadius: '0.5em'}} />
         </Link>
     </div>;
 }
